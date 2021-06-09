05718727988aefdb602e9240df9dafdd123a55fe
jackdelv <jackdelv39@gmail.com>
2021-06-09 08:43:25 -0400

    fixed schema issue

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
-
+const {Schema} = mongoose;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new Schema({
