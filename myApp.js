8b510d2046d8926c5ea9a2216c9e866777d6aeb1
jackdelv <jackdelv39@gmail.com>
2021-06-09 08:02:31 -0400

    Fixed Person Schema

--- myApp.js
+++ myApp.js
@@ -5,7 +5,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const personSchema = new Schema({
 	name: { type: String, required: true },
-	age: Number
+	age: Number,
+	favoriteFoods: [String]
 });
 
 const Person =  mongoose.model('Person', personSchema);
