d2247260a28bf81b5848e1204e1e3dc8fad371b4
jackdelv <jackdelv39@gmail.com>
2021-06-10 12:27:58 -0400

    Successfully created a person and added it to the database.

--- myApp.js
+++ myApp.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
-const {Schema} = mongoose;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const personSchema = new Schema({
+const personSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	age: Number,
 	favoriteFoods: [String]
 });
 
-const Person =  mongoose.model('Person', personSchema);
+var Person =  mongoose.model('Person', personSchema);
 
 var createAndSavePerson = function(done) {
   var person = new Person({name: "Jack", age: 21, favoriteFoods: ["pizza", "ice cream", "cake"]});
